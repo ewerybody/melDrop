3573cad3e66b3e7067a36b59648fc421eefcd15a
ewerybody <ewerybody+github@gmail.com>
2018-04-05 12:14:47 +0200

    oops. I had some updates on the graph module

--- scripts/ezLib/graph.py
+++ scripts/ezLib/graph.py
@@ -1,26 +1,31 @@
-﻿import maya.cmds as m
+﻿from maya import cmds
+
 
 def createInfinityMM():
 	# pmn = 'graphEditor1GraphEdanimCurveEditorMenu' # standard rmb popupmenu
 	gEd = 'graphEditor1GraphEd' # graph editor name
-	if not m.control( gEd, ex=1 ):
-		print ('the graph editor: ' + str(gEd) + ' does not (yet) exist!! :/')
+	if not cmds.control(gEd, ex=True):
+		print('the graph editor: ' + str(gEd) + ' does not (yet) exist!! :/')
 		return
 	# our new popup
 	pup = 'tweakGraphEditPopup'
-	if not m.popupMenu( pup, ex=1 ):
-		m.popupMenu( pup, parent=gEd, markingMenu=1, ctrlModifier=1 )
+	if not cmds.popupMenu(pup, ex=True):
+		cmds.popupMenu(pup, parent=gEd, markingMenu=1, ctrlModifier=1)
 
-	m.popupMenu( pup, edit=1, deleteAllItems=1 )
-	m.setParent( pup, menu=1 )
+	cmds.popupMenu(pup, edit=True, deleteAllItems=True)
+	cmds.setParent(pup, menu=True)
 
 	# display infinities checkbox
-	cmd = 'from maya.cmds import animCurveEditor; animCurveEditor( "' +gEd+ '", e=1, displayInfinities=int(not animCurveEditor( "' +gEd+ '", q=1, displayInfinities=1 )) )'
-	m.menuItem( 'displayInfinitiesMenuItem', label='Display Infinities', checkBox=m.animCurveEditor( gEd, q=1, displayInfinities=1 ), c=cmd, radialPosition='N' )
+	cmd = ('from maya.cmds import animCurveEditor; '
+		   'animCurveEditor("{gEd}", e=True, displayInfinities=int(not '
+		   'animCurveEditor("{gEd}", q=True, displayInfinities=True)))'.format(gEd=gEd))
+	cmds.menuItem('displayInfinitiesMenuItem', label='Display Infinities',
+				  checkBox=cmds.animCurveEditor( gEd, q=1, displayInfinities=1 ),
+				  c=cmd, radialPosition='N')
 
-	m.menuItem( 'preInfinityMenuItem', label='< Pre Infinity', subMenu=True, parent=pup ) # radialPosition='W'
-	m.menuItem( 'postInfinityMenuItem', label='Post Infinity >', subMenu=True, parent=pup ) # , radialPosition='E'
-	m.menuItem( 'bothInfinityMenuItem', label='< Both Infinity >', subMenu=True, parent=pup ) # , radialPosition='S'
+	cmds.menuItem('preInfinityMenuItem', label='< Pre Infinity', subMenu=True, parent=pup) # radialPosition='W'
+	cmds.menuItem('postInfinityMenuItem', label='Post Infinity >', subMenu=True, parent=pup) # , radialPosition='E'
+	cmds.menuItem('bothInfinityMenuItem', label='< Both Infinity >', subMenu=True, parent=pup) # , radialPosition='S'
 
 	infType = ['cycle', 'linear', 'constant', 'cycleRelative', 'oscillate']
 	itemList = ['preInfinityMenuItem', 'postInfinityMenuItem', 'bothInfinityMenuItem']
@@ -31,4 +36,4 @@ def createInfinityMM():
 				cmd += 'setInfinity( poi="' + type + '" );'
 			if i != 1:
 				cmd += 'setInfinity( pri="' + type + '" );'
-			m.menuItem( label=type, parent=itemList[i], c=cmd )
+			cmds.menuItem(label=type, parent=itemList[i], c=cmd)
