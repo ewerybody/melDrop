800ffb26fb1f654d85078e344cbd2c4d449db732
ewerybody <ewerybody+github@gmail.com>
2018-04-05 12:15:12 +0200

    TIL: zipfile uses no compression by default 🤦

--- scripts/melDrop/pacmel.py
+++ scripts/melDrop/pacmel.py
@@ -16,10 +16,12 @@ which decodes, extracts to temp and calls the snippet with a list of the files g
 @author: eric
 """
 import os
-import base64
 import uuid
+import base64
 import zipfile
-from os.path import join, abspath, dirname, basename, exists
+
+
+_THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 def pac(files, mel, code=None, pack_flat=True):
@@ -27,14 +29,14 @@ def pac(files, mel, code=None, pack_flat=True):
     :param list files: list of files to bundle. Alternatively you can put tuples into the list
         which are like (source_path, path_in_zip). these ignore the "pack_flat" setting.
     :param str mel: target mel file path
-    :param str code: code snippet to execute after the extraction, if it's an existing path to a
-        .py-file it will be read.
+    :param str code: code snippet to execute after the extraction, if it's
+        an existing path to a .py-file it will be read.
     :param bool pack_flat: to strip the paths from the packed files so that they are in the zip
         root. False is the standard zip behavior which retains the path minus the drive letter.
     """
     if not isinstance(files, list):
         files = [files]
-    
+
     # zip the given files
     file_tuples = []
     for f in files:
@@ -43,49 +45,49 @@ def pac(files, mel, code=None, pack_flat=True):
             arcname = f[1]
         else:
             filepath = f
-            arcname = basename(f) if pack_flat else f
-        if not exists(filepath):
-            raise Exception('File not existent: "%s"' % filepath)
-            return
+            arcname = os.path.basename(f) if pack_flat else f
+        if not os.path.isfile(filepath):
+            raise RuntimeError('File not existent: "%s"' % filepath)
         else:
             file_tuples.append((filepath, arcname))
 
     tmp_zip_name = 'pacmel_%s.zip' % str(uuid.uuid4())
-    tmp_zip_path = join(os.getenv('TEMP'), tmp_zip_name)
-    with zipfile.ZipFile(tmp_zip_path, 'w') as tmpzip:
+    tmp_zip_path = os.path.join(os.getenv('TEMP'), tmp_zip_name)
+    with zipfile.ZipFile(tmp_zip_path, 'w', zipfile.ZIP_DEFLATED) as tmpzip:
         for filepath, arcname in file_tuples:
             tmpzip.write(filepath, arcname)
-    
+
     # encode into mel
     with open(tmp_zip_path, 'rb') as fobj:
         src_data = fobj.read()
     os.remove(tmp_zip_path)
     mel_string = base64.encodestring(src_data).replace('\n', '\\\n')
     mel_code = 'string $stuff = "%s";\n\n' % mel_string
-    
+
     # append and format python code
-    thisdir = dirname(abspath(__file__))
-    with open(join(thisdir, 'pacmel.template'), 'r') as fobj:
+    with open(os.path.join(_THIS_DIR, 'pacmel.template'), 'r') as fobj:
         py_code = fobj.read()
-    
+
     if code is None:
-        with open(join(thisdir, 'pacmel.dummy.template'), 'r') as fobj:
+        with open(os.path.join(_THIS_DIR, 'pacmel.dummy.template'), 'r') as fobj:
             code = fobj.read()
-    elif exists(code):
+    elif os.path.isfile(code):
         with open(code, 'r') as fobj:
             code = fobj.read()
+
+    # make the code snippet work through the packing
     code = code.strip().replace('\n', '\n    ')
     code = code.replace('\\', '\\\\')
     code = code.replace('"', '\\')
 
     py_code = py_code.format(zipname=tmp_zip_name,
                              code=code)
-    
+
     py_code = py_code.replace('\r\n', '\\n\\\n')
-    
+
     c = """python("%s");""" % py_code
     mel_code += c + '\n'
-    
+
     # write the mel file
     with open(mel, 'w') as fobj:
         fobj.write(mel_code)
@@ -95,17 +97,15 @@ def _pac_yourself():
     """
     packs the necessary files of itself into a pacmel.mel
     """
-    _thisdir = dirname(abspath(__file__))
-    
     # provide a code snippet to execute
-    install_code = join(_thisdir, 'pacmel_dropcode.py')
+    install_code = os.path.join(_THIS_DIR, 'pacmel_dropcode.py')
     # assemble a bunch of files
     files = [__file__,
-             join(_thisdir, 'pacmel.template'),
-             join(_thisdir, 'pacmel.dummy.template'),
+             os.path.join(_THIS_DIR, 'pacmel.template'),
+             os.path.join(_THIS_DIR, 'pacmel.dummy.template'),
              install_code]
     # create a path to target mel file
-    mel = join(_thisdir, 'pacmel.mel')
+    mel = os.path.join(_THIS_DIR, 'pacmel.mel')
     # and fire it of:
     pac(files, mel, install_code)
     return mel
