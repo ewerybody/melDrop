691c7e0c4f157be09849bf0d301c8b9e862bbd96
Eric Werner <ewerybody+github@gmail.com>
2018-08-02 14:26:45 +0200

    just dropped the capital d

--- scripts/userSetup.py
+++ scripts/userSetup.py
@@ -1,4 +1,4 @@
 import os
-import melDrop
-print('\nmelDrop : booting from: "%s" ...' % os.path.dirname(melDrop.__file__))
-melDrop.start()
+import meldrop
+print('\nmelDrop : booting from: "%s" ...' % os.path.dirname(meldrop.__file__))
+meldrop.start()
